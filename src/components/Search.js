23fa8a72b32bea4e37ef75a49e5b713b6c6d9a9c
Daniel <admin@Admins-MacBook-Pro.local>
2022-10-26 14:38:12 -0400

    added filter buttons to home screen

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,47 @@
 import React from "react";
 import { StyleSheet, View, Text, Pressable } from "react-native";
 import { useFonts } from "expo-font";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
-import IonIcon from "react-native-vector-icons/Ionicons";
 export default function Search({ handlePress }) {
   const [loaded] = useFonts({
     UrbanistRegular: require("../../assets/fonts/urbanist/Urbanist-Regular.ttf"),
     UrbanistLight: require("../../assets/fonts/urbanist/Urbanist-Light.ttf"),
+    UrbanistSemiBold: require("../../assets/fonts/urbanist/Urbanist-SemiBold.ttf"),
   });
 
   if (!loaded) {
     return null;
   }
   return (
-    <View style={styles.searchContainer}>
-      <View style={styles.iconContainer}>
-        <IonIcon name="search" size={24} />
+    <View style={styles.wrapper}>
+      <View style={styles.searchContainer}>
+        <View style={styles.iconContainer}>
+          <Ionicons name="search" size={24} />
+        </View>
+        <Pressable style={styles.textInput} onPress={handlePress}>
+          <Text style={styles.text}>Service, Stylist, or Product</Text>
+        </Pressable>
       </View>
-      <Pressable style={styles.textInput} onPress={handlePress}>
-        <Text style={styles.text}>Service, Stylist, or Product</Text>
-      </Pressable>
+      <View style={styles.filterWrapper}>
+        <Pressable style={[styles.btn, { marginRight: 10 }]}>
+          <Ionicons name="md-map" size={24} />
+          <Text style={styles.btnText}>Map</Text>
+        </Pressable>
+        <Pressable style={styles.btn}>
+          <AntDesign name="star" size={24} />
+          <Text style={styles.btnText}>Rating</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    marginBottom: 24,
+  },
   searchContainer: {
     marginVertical: 24,
     marginHorizontal: 24,
@@ -52,5 +69,23 @@ const styles = StyleSheet.create({
     fontFamily: "UrbanistRegular",
     fontSize: 16,
     color: "#adadad",
+  },
+  filterWrapper: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    paddingRight: 24,
+  },
+  btn: {
+    backgroundColor: "#ededed",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    width: 100,
+    borderRadius: 7,
+    flexDirection: "row",
+  },
+  btnText: {
+    fontFamily: "UrbanistSemiBold",
+    marginLeft: 7,
   },
 });
